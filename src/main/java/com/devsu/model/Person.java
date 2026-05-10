15d7b8a293174ef1d609f86c0e2059e22f0b69ad
John Gomez <gomezjohn@tuta.com>
2026-05-10 10:34:38 -0400

    Person and Client changes
    Adding some stuff to the client and person structure for the project to work properly

--- src/main/java/com/devsu/model/Person.java
+++ src/main/java/com/devsu/model/Person.java
@@ -5,16 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @MappedSuperclass
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public abstract class Person {
 
     @Id
@@ -36,4 +28,37 @@ public abstract class Person {
     private String address;
 
     private String phoneNumber;
+
+    public Person() {}
+
+    public Person(Long id, String name, String gender, Integer age, String identification, String address, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.identification = identification;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public Integer getAge() { return age; }
+    public void setAge(Integer age) { this.age = age; }
+
+    public String getIdentification() { return identification; }
+    public void setIdentification(String identification) { this.identification = identification; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 }
